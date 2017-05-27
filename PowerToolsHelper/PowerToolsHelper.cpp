cc97a70d37da1e507c59e09507890372973f6526
Darren A. Anderson <darrenaa@outlook.com>
2017-05-27 00:26:16 -0600

    Convert tabs to spaces

--- PowerToolsHelper/PowerToolsHelper.cpp
+++ PowerToolsHelper/PowerToolsHelper.cpp
@@ -16,110 +16,110 @@ using namespace System;
 
 namespace PowerToolsHelper
 {
-	//InstalledProduct contains information related to installed products
-	// on the windows platform (information is retrieved using the Msi.dll API)
-	public ref struct InstalledProduct
-	{
-	public:
-		property System::String^ m_strGUID; //Product GUID
-		property System::String^ m_strName; //Product Name
-		property System::String^ m_strPath; //Installed To Path
-		property System::UInt32^ m_uiVersionMajor; //Product Major Version
-		property System::UInt32^ m_uiVersionMinor; //Product Minor Version
-	};
+    //InstalledProduct contains information related to installed products
+    // on the windows platform (information is retrieved using the Msi.dll API)
+    public ref struct InstalledProduct
+    {
+    public:
+        property System::String^ m_strGUID; //Product GUID
+        property System::String^ m_strName; //Product Name
+        property System::String^ m_strPath; //Installed To Path
+        property System::UInt32^ m_uiVersionMajor; //Product Major Version
+        property System::UInt32^ m_uiVersionMinor; //Product Minor Version
+    };
 
-	public ref class PowerToolsHelper
-	{
-	public:
-		static array<InstalledProduct^>^ GetListOfInstalledProducts()
-		{
-			//C++ objects
-			int iProducts = 0;
-			wchar_t wszProductGUID[64] = { };
-			std::vector<std::wstring> vec_str_Products;
-			wchar_t wszReturnedValue[2048];
-			ulong_t ulBufferSize = 2048;
+    public ref class PowerToolsHelper
+    {
+    public:
+        static array<InstalledProduct^>^ GetListOfInstalledProducts()
+        {
+            //C++ objects
+            int iProducts = 0;
+            wchar_t wszProductGUID[64] = { };
+            std::vector<std::wstring> vec_str_Products;
+            wchar_t wszReturnedValue[2048];
+            ulong_t ulBufferSize = 2048;
 
-			//CLI garbage collected objects
-			array<InstalledProduct^>^ arrInstalledProducts = nullptr;
+            //CLI garbage collected objects
+            array<InstalledProduct^>^ arrInstalledProducts = nullptr;
 
-			//Get the GUIDs for all the installed products
-			while (ERROR_SUCCESS == MsiEnumProductsW(iProducts, wszProductGUID))
-			{
-				vec_str_Products.push_back(std::wstring(wszProductGUID));
-				iProducts++;
-			}
+            //Get the GUIDs for all the installed products
+            while (ERROR_SUCCESS == MsiEnumProductsW(iProducts, wszProductGUID))
+            {
+                vec_str_Products.push_back(std::wstring(wszProductGUID));
+                iProducts++;
+            }
 
-			//initialize and allocate the array of InstalledProduct 
-			// to match the number of product GUIDs returned
-			arrInstalledProducts = gcnew array<InstalledProduct^>(iProducts);
+            //initialize and allocate the array of InstalledProduct 
+            // to match the number of product GUIDs returned
+            arrInstalledProducts = gcnew array<InstalledProduct^>(iProducts);
 
-			//Get the installation information for the list of product GUIDs
-			for (int iProduct = 0; iProduct < iProducts; iProduct++)
-			{
-				//Create a new InstalledProduct object
-				arrInstalledProducts[iProduct] = gcnew InstalledProduct();
+            //Get the installation information for the list of product GUIDs
+            for (int iProduct = 0; iProduct < iProducts; iProduct++)
+            {
+                //Create a new InstalledProduct object
+                arrInstalledProducts[iProduct] = gcnew InstalledProduct();
 
-				//Assign the GUID
-				arrInstalledProducts[iProduct]->m_strGUID = gcnew System::String(vec_str_Products[iProduct].data());
+                //Assign the GUID
+                arrInstalledProducts[iProduct]->m_strGUID = gcnew System::String(vec_str_Products[iProduct].data());
 
-				//Set the return buffer size 
-				ulBufferSize = 2048;
+                //Set the return buffer size 
+                ulBufferSize = 2048;
 
-				//Get the name of the product
-				if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
-														INSTALLPROPERTY_PRODUCTNAME,
-														wszReturnedValue,
-														&ulBufferSize))
-				{
-					arrInstalledProducts[iProduct]->m_strName = gcnew System::String(wszReturnedValue, 0, ulBufferSize);
-				}
+                //Get the name of the product
+                if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
+                                                        INSTALLPROPERTY_PRODUCTNAME,
+                                                        wszReturnedValue,
+                                                        &ulBufferSize))
+                {
+                    arrInstalledProducts[iProduct]->m_strName = gcnew System::String(wszReturnedValue, 0, ulBufferSize);
+                }
 
-				//Set the return buffer size
-				ulBufferSize = 2048;
+                //Set the return buffer size
+                ulBufferSize = 2048;
 
-				//Get the installation path for the product
-				if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
-														INSTALLPROPERTY_INSTALLLOCATION,
-														wszReturnedValue,
-														&ulBufferSize))
-				{
-					arrInstalledProducts[iProduct]->m_strPath = gcnew System::String(wszReturnedValue, 0, ulBufferSize);
-				}
-				else
-				{
-					System::Console::WriteLine(L"Failed to Get Install Path");
-				}
+                //Get the installation path for the product
+                if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
+                                                        INSTALLPROPERTY_INSTALLLOCATION,
+                                                        wszReturnedValue,
+                                                        &ulBufferSize))
+                {
+                    arrInstalledProducts[iProduct]->m_strPath = gcnew System::String(wszReturnedValue, 0, ulBufferSize);
+                }
+                else
+                {
+                    System::Console::WriteLine(L"Failed to Get Install Path");
+                }
 
-				//Set the return buffer size
-				ulBufferSize = 2048;
+                //Set the return buffer size
+                ulBufferSize = 2048;
 
-				//Get the major version for the product
-				if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
-														INSTALLPROPERTY_VERSIONMAJOR,
-														wszReturnedValue,
-														&ulBufferSize))
-				{
-					arrInstalledProducts[iProduct]->m_uiVersionMajor = gcnew System::UInt32(std::stoul(wszReturnedValue));
-				}
+                //Get the major version for the product
+                if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
+                                                        INSTALLPROPERTY_VERSIONMAJOR,
+                                                        wszReturnedValue,
+                                                        &ulBufferSize))
+                {
+                    arrInstalledProducts[iProduct]->m_uiVersionMajor = gcnew System::UInt32(std::stoul(wszReturnedValue));
+                }
 
-				//Set the return buffer size
-				ulBufferSize = 2048;
+                //Set the return buffer size
+                ulBufferSize = 2048;
 
-				//Get the minor version for the product
-				if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
-														INSTALLPROPERTY_VERSIONMINOR,
-														wszReturnedValue,
-														&ulBufferSize))
-				{
-					arrInstalledProducts[iProduct]->m_uiVersionMinor = gcnew System::UInt32(std::stoul(wszReturnedValue));
-				}
-			}//end for iProduct
+                //Get the minor version for the product
+                if (ERROR_SUCCESS == MsiGetProductInfoW(vec_str_Products[iProduct].data(),
+                                                        INSTALLPROPERTY_VERSIONMINOR,
+                                                        wszReturnedValue,
+                                                        &ulBufferSize))
+                {
+                    arrInstalledProducts[iProduct]->m_uiVersionMinor = gcnew System::UInt32(std::stoul(wszReturnedValue));
+                }
+            }//end for iProduct
 
-			return arrInstalledProducts;
+            return arrInstalledProducts;
 
-		}//end GetListOfInstalledProducts
-	};
+        }//end GetListOfInstalledProducts
+    };
 
 
 
